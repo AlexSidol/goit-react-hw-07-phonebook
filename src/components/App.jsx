044393444f9715404771178ea325b63cc2e2b9b3
AlexSidol <asidolaka@gmail.com>
2023-01-22 23:03:36 +0200

    changed logic
    changed logic

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,42 @@
-export const App = () => {
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { selectContacts, selectIsLoading, selectError } from 'redux/selectors';
+
+import ContactForm from './ContactForm/ContactForm';
+import Filter from './Filter/Filter';
+import ContactList from './ContactList/ContactList';
+import css from '../components/App.module.css';
+import { fetchContacts } from 'redux/operation';
+import { useEffect } from 'react';
+
+export default function App() {
+  const contacts = useSelector(selectContacts);
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.phonebook__form}>
+      <ToastContainer position="top-center" />
+      <h1>Phonebook</h1>
+      <ContactForm />
+
+      <h2>Contacts</h2>
+      {isLoading && !error && <p>Sorry, but i have paws</p>}
+      {contacts.length > 0 ? (
+        <div>
+          <Filter />
+          <ContactList />
+        </div>
+      ) : (
+        <p>Please, add contact ☝️</p>
+      )}
     </div>
   );
-};
+}
